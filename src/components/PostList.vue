e64d91e4e04ffc2ec07c1d85734621dd907e4773
enextus <enextus@gmail.com>
2022-02-15 12:50:59 +0100

    decomposition ListItem 15.02.2022

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,10 +1,24 @@
 <template>
+  <div>
+    <h3>User list</h3>
+    <post-item
+      v-for="post in posts"
+      :post="post"
+
+    >
+      <!--      <div><strong>Post name:</strong> {{ post.title }}</div>
+            <div><strong>Description:</strong> {{ post.body }}</div>-->
+    </post-item>
+  </div>
 
 </template>
 
 
 <script>
+import PostItem from "@/components/PostItem";
+
 export default {
+  components: {PostItem},
   props: {
     posts: {
       type: Array,
